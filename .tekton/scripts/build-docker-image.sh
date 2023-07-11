4744b0956558ec35115e971f2300337c960cdd96
Diego Gonzalez Gualteros <diego.gonzalez.gualteros@ibm.com>
2023-07-11 11:33:20 -0500

    cambio en buildctl build

--- .tekton/scripts/build-docker-image.sh
+++ .tekton/scripts/build-docker-image.sh
@@ -89,6 +89,7 @@ set -x
 buildctl build \
     --frontend dockerfile.v0 --opt filename=${DOCKER_FILE} --local dockerfile=${DOCKER_ROOT} \
     ${BUILD_ARGS} --local context=${DOCKER_ROOT} \
+    --driver-opt network=host \
     --import-cache type=registry,ref=${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME} \
     --output type=image,name="${REGISTRY_URL}/${REGISTRY_NAMESPACE}/${IMAGE_NAME}:${IMAGE_TAG}",push=true
 set +x
